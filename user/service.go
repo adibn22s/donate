5d1dd614977654355322dfc822e755c966caec78
adibn22s <adib.shodiq@gmail.com>
2023-08-05 19:08:04 +0700

    auth middleware

--- user/service.go
+++ user/service.go
@@ -11,6 +11,7 @@ type Service interface {
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
 	SaveAvatar(ID int, fileLocation string) (User, error)
+	GetuUserByID(ID int) (User, error)
 }
 
 type service struct {
@@ -78,23 +79,37 @@ func (s *service) IsEmailAvailable(input CheckEmailInput) (bool, error){
 
 		return false, nil
 
-	}
+}
 
 	
-	func (s *service) SaveAvatar(ID int, fileLocation string) (User, error){
+func (s *service) SaveAvatar(ID int, fileLocation string) (User, error){
 
-		user, err := s.repository.FindByID(ID)
-		if err != nil {
-			return user, err
-		}
-
-		user.AvatarFileName = fileLocation
-
-		updatedUser, err := s.repository.Update(user)
-		if err != nil {
-			return updatedUser, err
-		}
-
-		return updatedUser, nil
-
+	user, err := s.repository.FindByID(ID)
+	if err != nil {
+		return user, err
 	}
+
+	user.AvatarFileName = fileLocation
+
+	updatedUser, err := s.repository.Update(user)
+	if err != nil {
+		return updatedUser, err
+	}
+
+	return updatedUser, nil
+
+}
+
+func (s *service) GetuUserByID(ID int) (User, error) {
+	user, err := s.repository.FindByID(ID)
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("User Not Found")
+	}
+	
+	return user, nil
+
+}
