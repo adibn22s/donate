829ca9fdf5b390374ceecc2b9cc2006fd0e61e33
adibn22s <adib.shodiq@gmail.com>
2023-08-08 20:39:22 +0700

    get user transaction endpoint

--- transaction/repository.go
+++ transaction/repository.go
@@ -8,6 +8,7 @@ type repository struct {
 
 type Repository interface {
 	GetCampaignByID(campaignID int) ([]Transaction, error)
+	GetByUserID(userID int) ([]Transaction, error)
 }
 
 func NewRepository(db *gorm.DB) *repository {
@@ -18,6 +19,16 @@ func (r *repository) GetCampaignByID(CampaignID int) ([]Transaction, error){
 	var transactions []Transaction
 
 	err := r.db.Preload("User").Where("campaign_id = ?", CampaignID).Order("id desc").Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
+
+func (r *repository) GetByUserID(userID int) ([]Transaction, error){
+	var transactions []Transaction
+
+	err := r.db.Preload("Campaign.CampaignImages","campaign_images.is_primary = 1").Where("user_id = ?", userID).Order("id desc").Find(&transactions).Error
 	if err != nil {
 		return transactions, err
 	}
